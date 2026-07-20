92131c1d4d44ccab459df7e1158794a2a9174011
Tungbm <minh.95.tung@gmail.com>
2026-07-20 16:55:01 +0900

    video 18,19

---
+++ src/video18/video18.js
@@ -0,0 +1,13 @@
+console.log("video 18")
+
+//number
+const age = 25;
+const myScore = 9.1;
+
+console.log(myScore, age);
+
+//string 
+const name = "tungbm123";
+const name1 = 'src';
+
+console.log(name, name1);
