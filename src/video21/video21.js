e97f75b81e57f9dd05e99825925be660cd89d457
Tungbm <minh.95.tung@gmail.com>
2026-07-20 19:22:24 +0900

    update

---
+++ src/video21/video21.js
@@ -0,0 +1,8 @@
+
+console.log("video 21");
+console.log("info")
+console.error("ooppps");
+console.warn("test warning");
+
+
+console.log("%cCanh bao!", "color:red; font-weight: bold");
---
+++ src/video22/video22.js
@@ -0,0 +1,7 @@
+console.log("video 22");
+
+const name = "Bui Minh Tung";
+const age = 30;
+
+const intro = `my name is ${name} and I'm ${age} years old`;
+console.log(intro);
